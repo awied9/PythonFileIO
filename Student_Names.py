f26dd379b7c31e4ebfb9c6060a041746880f2e48
awied9 <anna.wied@yahoo.com>
2023-09-03 14:20:33 -0500

    update studentNames

--- Student_Names.py
+++ Student_Names.py
@@ -1,16 +1,25 @@
-s = open('studentnames.txt', 'r')
-numStudents = 0
-for student in s:
-    name = student.rstrip('\n')
-    if len(name) > 0:
-        print(name)
-        numStudents += 1
-print("There were " + str(numStudents) + " students in the file.")
-# print(s.readline().rstrip('\n'))
-# print(s.readline().rstrip('\n'))
-# print(s.readline().rstrip('\n'))
-# print(s.readline().rstrip('\n'))
-# print(s.readline().rstrip('\n'))
-# print(s.readline().rstrip('\n'))
+# s = open('studentnames.txt', 'r')
+# numStudents = 0
+# for student in s:
+#     name = student.rstrip('\n')
+#     if len(name) > 0:
+#         print(name)
+#         numStudents += 1
+# print("There were " + str(numStudents) + " students in the file.")
+# print(s.readline().rstrip('\n')+"k")
+# # print(s.readline().rstrip('\n'))
+# # print(s.readline().rstrip('\n'))
+# # print(s.readline().rstrip('\n'))
+# # print(s.readline().rstrip('\n'))
+# # print(s.readline().rstrip('\n'))
+#
+# s.close()
 
+s = open('StudentNames.txt', 'r')
+for student in s:
+    name = student.strip('\n')
+    if(len(name)>0):
+        print(name)
+
+print("There are " )
 s.close()
